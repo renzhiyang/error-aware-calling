f64075a31d13980914cac63fa73c8208cfb0b9c1
renzhiyang <renzhiyang1031@gmail.com>
2024-02-19 18:43:22 +0900

    data loader update

--- src/error-prediction/data_loader.py
+++ src/error-prediction/data_loader.py
@@ -20,16 +20,38 @@ def read_vcf(filename):
             variants[pos] = {'chr': chr, 'ref': ref, 'alts': alts, 'is_indel': is_indel}
     return variants
 
+
+# Load confident regions
+def read_bed(filename):
+    regions = {}
+    open_func = gzip.open if filename.endswith('.gz') else open
+    with open_func(filename, 'rt') as file:
+        for line in file:
+            if line.startswith('#') or line.strip() == '':
+                continue
+            columns = line.strip().split('\t')
+            chr, start_pos, end_pos = columns[0], int(columns[1]), int(columns[2])
+            if chr not in regions:
+                regions[chr] = []
+            regions[chr].append((start_pos, end_pos))
+    return regions
+            
+
 # The function to identify if the current base in sequencing read support ref/variant or not.
 # If the current base not support ref/variant, it will be recongnized as sequencing error.
 # It only work in highly condident regions, errors are used to build training data
 def is_error(position, base, is_indel):
     return None
 
+
 def build_training_label(cfg):
     bam_file = pysam.AlignmentFile(cfg.data_path.bam_f)
+    confident_regions = read_bed(cfg.data_path.confident_f)
+    print(confident_regions)
+    return 
+    variants = read_vcf(cfg.data_path.vcf_f)
     for read in bam_file:
-        if read.mapping_quality <= cfg.alignment.min_mapping_quality:
+        if read.mapping_quality <= cfg.alignment_filter.min_mapping_quality:
             continue
         return
     
@@ -37,9 +59,7 @@ def build_training_label(cfg):
 
 @hydra.main(version_base=None, config_path='../../configs/error-prediction', config_name='params.yaml')
 def main(cfg: DictConfig) -> None:
-    build_training_label(cfg)
-    return 
-    variants = read_vcf(cfg.data_path.vcf_f)
+    build_training_label(cfg) 
 
 
 if __name__ == '__main__':
